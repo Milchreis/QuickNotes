00ed1fa7f41bc5a0b63c465f1a16f64f6974e9a5
Milchreis <nick-m@gmx.de>
2019-10-24 22:00:55 +0200

    Removes footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -39,12 +39,6 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019 - Milchreis</span>
-    </v-footer>
   </v-app>
 </template>
 
